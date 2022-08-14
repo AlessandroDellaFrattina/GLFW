1708f94da854a2e68ca272f2afdc548f53dfa377
Alilux <83576617+AlessandroDellaFrattina@users.noreply.github.com>
2022-08-15 01:39:27 +0200

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -12,6 +12,12 @@ project "GLFW"
 		"src/glfw_config.h",
 		"src/context.c",
 		"src/init.c",
+		"src/null_init.c",
+		"src/null_joystick.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_init.c",
+		"src/platform.c",
 		"src/input.c",
 		"src/monitor.c",
 		"src/vulkan.c",
@@ -20,6 +26,7 @@ project "GLFW"
 
 	filter "system:linux"
 		pic "On"
+
 		systemversion "latest"
 		staticruntime "On"
 
@@ -51,6 +58,7 @@ project "GLFW"
 			"src/win32_init.c",
 			"src/win32_joystick.c",
 			"src/win32_monitor.c",
+			"src/win32_module.c",
 			"src/win32_time.c",
 			"src/win32_thread.c",
 			"src/win32_window.c",
