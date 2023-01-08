6c16dd8a9ce332c4bab0b3deb191b75f6fef9476
asmdenc1 <alessandrodellafrattina@gmail.com>
2023-01-08 14:03:55 +0100

    Updated premake5.lua (for macOS)

--- premake5.lua
+++ premake5.lua
@@ -28,7 +28,8 @@ project "GLFW"
 		"src/monitor.c",
 		"src/vulkan.c",
 		"src/window.c",
-		"src/osmesa_context.c"
+		"src/osmesa_context.c",
+		"src/egl_context.c"
 	}
 
 	filter "system:windows"
@@ -50,8 +51,7 @@ project "GLFW"
 			"src/win32_time.c",
 			"src/win32_thread.c",
 			"src/win32_window.c",
-			"src/wgl_context.c",
-			"src/egl_context.c"
+			"src/wgl_context.c"
 		}
 
 	filter "system:linux"
@@ -69,7 +69,6 @@ project "GLFW"
 			"src/posix_time.c",
 			"src/posix_thread.c",
 			"src/glx_context.c",
-			"src/egl_context.c",
 			"src/linux_joystick.c"
 		}
 
@@ -92,7 +91,10 @@ project "GLFW"
 			"src/cocoa_joystick.m",
 			"src/cocoa_monitor.m",
 			"src/cocoa_time.c",
-			"src/cocoa_window.m"
+			"src/cocoa_window.m",
+			"src/posix_thread.c",
+			"src/posix_module.c",
+			"nsgl_context.m"
 		}
 
 	filter "configurations:Debug"
